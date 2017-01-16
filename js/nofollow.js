010c29bc99857dfba82780da5de1c68dc42cab53
Yuji Funakoshi <funakoshi.yuji@gmail.com>
2017-01-16 20:04:14 +0900

    add chrome-extension
    first commit

---
+++ js/nofollow.js
@@ -0,0 +1,31 @@
+$(function(){
+    // メタタグチェック
+    var robots = $("meta[name=robots]").attr("content");
+    console.log(robots);
+    if(robots === undefined) {
+        robots = "";
+    }
+    var noindex = robots.match(/noindex/i);
+    if(noindex !== null) {
+        noindex = "true";
+    } else {
+        noindex = "false";
+    }
+    var nofollow = robots.match(/nofollow/i);
+    if(nofollow !== null) {
+        nofollow = "true";
+    } else {
+        nofollow = "false";
+    }
+
+    var content = $('<div></div>', {
+        "id": "chrome_nofollow_notify",
+        "html": "META-Robots NoIndex: <span class='chrome-nofollow-text-"+noindex+"'>"+noindex+"</span>"+
+            "<br>META-Robots NoFollow: <span class='chrome-nofollow-text-"+nofollow+"'>"+nofollow+"</span>"
+    });
+    $("body").append(content);
+
+    // aリンクチェック
+    var className = "chrome-nofollow-link";
+    $('a[rel*="nofollow"]').addClass(className);
+});
