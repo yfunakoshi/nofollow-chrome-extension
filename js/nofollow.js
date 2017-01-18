2f4a1d98afad38f1c687a9721015e410baeb59ad
Yuji Funakoshi <funakoshi.yuji@gmail.com>
2017-01-18 19:55:05 +0900

    add disable options

--- js/nofollow.js
+++ js/nofollow.js
@@ -1,31 +1,39 @@
 $(function(){
-    // メタタグチェック
-    var robots = $("meta[name=robots]").attr("content");
-
-    if(robots === undefined) {
-        robots = "";
-    }
-    var noindex = robots.match(/noindex/i);
-    if(noindex !== null) {
-        noindex = "true";
-    } else {
-        noindex = "false";
-    }
-    var nofollow = robots.match(/nofollow/i);
-    if(nofollow !== null) {
-        nofollow = "true";
-    } else {
-        nofollow = "false";
-    }
-
-    var content = $('<div></div>', {
-        "id": "chrome_nofollow_notify",
-        "html": "META-Robots NoIndex: <span class='chrome-nofollow-text-"+noindex+"'>"+noindex+"</span>"+
-            "<br>META-Robots NoFollow: <span class='chrome-nofollow-text-"+nofollow+"'>"+nofollow+"</span>"
+    chrome.storage.local.get("enable", function(items) {
+        if(items.enable === "enable") {
+            metacheck();
+        }
     });
-    $("body").append(content);
 
-    // aリンクチェック
-    var className = "chrome-nofollow-link";
-    $('a[rel*="nofollow"]').addClass(className);
+    var metacheck = function(){
+        // メタタグチェック
+        var robots = $("meta[name=robots]").attr("content");
+
+        if(robots === undefined) {
+            robots = "";
+        }
+        var noindex = robots.match(/noindex/i);
+        if(noindex !== null) {
+            noindex = "true";
+        } else {
+            noindex = "false";
+        }
+        var nofollow = robots.match(/nofollow/i);
+        if(nofollow !== null) {
+            nofollow = "true";
+        } else {
+            nofollow = "false";
+        }
+
+        var content = $('<div></div>', {
+            "id": "chrome_nofollow_notify",
+            "html": "META-Robots NoIndex: <span class='chrome-nofollow-text-"+noindex+"'>"+noindex+"</span>"+
+            "<br>META-Robots NoFollow: <span class='chrome-nofollow-text-"+nofollow+"'>"+nofollow+"</span>"
+        });
+        $("body").append(content);
+
+        // aリンクチェック
+        var className = "chrome-nofollow-link";
+        $('a[rel*="nofollow"]').addClass(className);
+    }
 });
---
+++ js/popup.js
@@ -0,0 +1,18 @@
+$(function(){
+    chrome.storage.local.get(['enable'], function(items) {
+        if(items.enable == "enable") {
+            $("#enable").val("disable");
+        } else {
+            $("#enable").val("enable");
+        }
+    });
+
+    $("#enable").click(function(){
+        chrome.storage.local.set({"enable": $("#enable").val()});
+        if($("#enable").val() == "disable") {
+            $(this).val("enable");
+        } else {
+            $(this).val("disable");
+        }
+    });
+});
